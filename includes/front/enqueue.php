3afaa2d79a2cb31d083458d984c487208541195e
Lobe Musonda <Lo.Musonda@pacra.org.zm>
2022-08-31 20:23:47 +0200

    Managing asset files complete

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -2,5 +2,22 @@
 
 function gt_enqueue()
 {
+    wp_register_style(
+        'gt_font_rubik_and_pacifico',
+        'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap',
+        [],
+        null
+    );
+    wp_register_style(
+        'gt_bootstrap_icons',
+        get_theme_file_uri('assets/bootstrap-icons/bootstrap-icons.css')
+    );
+    wp_register_style(
+        'gt_theme',
+        get_theme_file_uri('assets/public/index.css')
+    );
 
+    wp_enqueue_style('gt_font_rubik_and_pacifico');
+    wp_enqueue_style('gt_bootstrap_icons');
+    wp_enqueue_style('gt_theme');
 }

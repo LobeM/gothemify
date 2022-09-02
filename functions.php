6e1496d78f98ff3b17099bfb6bcb3c915cda08ee
Lobe Musonda <lobe350@gmail.com>
2022-09-02 06:25:56 +0200

    Finished header

--- functions.php
+++ functions.php
@@ -5,7 +5,9 @@
 // Includes
 include get_theme_file_path('/includes/front/enqueue.php');
 include get_theme_file_path('/includes/front/head.php');
+include get_theme_file_path('/includes/setup.php');
 
 // Hooks
 add_action('wp_enqueue_scripts', 'gt_enqueue');
 add_action('wp_head', 'gt_head', 5);
+add_action('after_setup_theme', 'gt_setup_theme');
